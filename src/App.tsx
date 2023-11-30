d411264292b20e1981fead42f663370c2129859d
StarT_T <novikov_roman.92@mail.ru>
2023-12-01 01:51:48 +0300

    Промежуточный коммит

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,62 @@
-import React, {useEffect} from "react";
-import HeroBundle from "./components/HeroBundle/HeroBundle";
-import {useGetHeroClassesData} from "./dataHooks/useGetHeroClassesData";
+import React from "react";
+import {useGetHeroClassesData} from "./dataHooks";
 import {useHeroesDataDispatchContext} from "./context/heroes-data-context";
 import {DataReducerActionsEnum} from "./context/heroes-data-context-reducer";
 import LoadingSpinner from "./components/LoadingSpinner/LoadingSpinner";
-import {useGetHeroesData} from "./dataHooks/useGetHeroesData";
-import {useGetItemsData} from "./dataHooks/useGetItemsData";
-import {Routes, Route} from "react-router-dom";
+import {useGetHeroesData} from "./dataHooks";
+import {useGetItemsData} from "./dataHooks";
+import {Route, Routes,} from "react-router-dom";
+import HeroSheet from "./components/HeroBundle/HeroSheet/HeroSheet";
+import {ChoosePlayerButtons} from "./components/ChoosePlayerButtons/ChoosePlayerButtons";
+import {OverlordBench} from "./components/OverlordBench/OverlordBench";
+import {useGetCampaignsData} from "./dataHooks";
 
 export const App = () => {
 
+    const {data} = useGetCampaignsData();
+    console.log(data)
+
     const {data: heroesData, isSuccess: heroesDataSuccess, isLoading: heroesDataIsLoading} = useGetHeroesData();
-    const {data: heroClassesData, isSuccess: heroClassesDataSuccess, isLoading: heroClassesIsLoading} = useGetHeroClassesData();
+    const {
+        data: heroClassesData,
+        isSuccess: heroClassesDataSuccess,
+        isLoading: heroClassesIsLoading
+    } = useGetHeroClassesData();
     const {data: itemsData, isSuccess: itemsDataSuccess, isLoading: itemsDataLoading} = useGetItemsData();
 
     const dispatch = useHeroesDataDispatchContext();
 
     const isLoading = heroesDataIsLoading || heroClassesIsLoading || itemsDataLoading;
+    const isSuccess = heroesDataSuccess && heroClassesDataSuccess && itemsDataSuccess;
 
-    useEffect(() => {
-        const isSuccess = heroesDataSuccess && heroClassesDataSuccess && itemsDataSuccess;
-        if (isSuccess) {
-            dispatch({payload: {
-                    heroes: heroesData || {},
-                    heroClasses: heroClassesData || {},
-                    items: itemsData || {},
-                }, actionType: DataReducerActionsEnum.update})
-        }
-    }, [heroesData, heroClassesData, itemsData])
+    if (isSuccess) {
+        dispatch({
+            payload: {
+                heroes: heroesData || {},
+                heroClasses: heroClassesData || {},
+                items: itemsData || {},
+            }, actionType: DataReducerActionsEnum.update
+        })
+    }
 
-    if(isLoading) {
-        return <LoadingSpinner />
+    if (isLoading) {
+        return <LoadingSpinner/>
     }
 
     return (
         <Routes>
-            <Route path="/" element={<HeroBundle />}>
-                <Route index element={<HeroBundle />} />
-                <Route path="about" element={<HeroBundle />} />
-                <Route path="dashboard" element={<HeroBundle />} />
-
+            <Route path="/" element={<ChoosePlayerButtons/>}>
                 {/* Using path="*"" means "match anything", so this route
                 acts like a catch-all for URLs that we don't have explicit
                 routes for. */}
-                <Route path="*" element={<HeroBundle />} />
+                <Route path="*" element={<div>error route</div>} />
             </Route>
+            <Route
+                path={'players/:playerRole'}
+                element={<HeroSheet />}/>
+            <Route
+                path={'players/overlord'}
+                element={<OverlordBench />}/>
         </Routes>
     )
 
