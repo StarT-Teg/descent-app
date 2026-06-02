214bff77cc43794516c8448eef70bc50dfd13732
StarT_T <novikov_roman.92@mail.ru>
2026-06-03 00:20:41 +0300

    Webp image conversion

--- src/shared/expansion-constants.ts
+++ src/shared/expansion-constants.ts
@@ -1,44 +1,44 @@
 // Box Expansions
-import conversionKitImg from '../assets/img/expansions/conversion-kit.jpg';
 import lairOfTheWyrmImg from '../assets/img/expansions/lair-of-the-wyrm.webp';
-import labyrinthOfRuinImg from '../assets/img/expansions/labyrinth-of-ruin.png';
-import theTrollFensImg from '../assets/img/expansions/the-trollfens.png';
-import manorOfRavensImg from '../assets/img/expansions/manor-of-ravens.png';
-import mistsOfBilehallImg from '../assets/img/expansions/mists-of-bilehall.png';
-import shardsOfEverdarkImg from '../assets/img/expansions/shards-of-everdark.png';
+import labyrinthOfRuinImg from '../assets/img/expansions/labyrinth-of-ruin.webp';
+import theTrollFensImg from '../assets/img/expansions/the-trollfens.webp';
+import manorOfRavensImg from '../assets/img/expansions/manor-of-ravens.webp';
+import mistsOfBilehallImg from '../assets/img/expansions/mists-of-bilehall.webp';
+import shardsOfEverdarkImg from '../assets/img/expansions/shards-of-everdark.webp';
 import shadowOfNerekhallImg from '../assets/img/expansions/ShadowOfNerekhall.webp';
 // Lieutenant Packs
-import elizaFarrowImg from '../assets/img/expansions/eliza-farrow.jpg';
-import spligImg from '../assets/img/expansions/splig.jpg';
-import zacharethImg from '../assets/img/expansions/zachareth.jpg';
-import ariadImg from '../assets/img/expansions/ariad.jpg';
-import alricFarrowImg from '../assets/img/expansions/alric-farrow.jpg';
-import valyndraImg from '../assets/img/expansions/valyndra.png';
-import queenAriadImg from '../assets/img/expansions/queen-ariad.jpg';
-import belthirImg from '../assets/img/expansions/belthir.jpg';
-import raythenImg from '../assets/img/expansions/raythen.png';
-import bolgorethImg from '../assets/img/expansions/bolgoreth.png';
-import tristayneOlivenImg from '../assets/img/expansions/tristayne-olliven.png';
-import serenaImg from '../assets/img/expansions/serena.png';
-import verminousImg from '../assets/img/expansions/verminous.png';
-import garganMirklaceImg from '../assets/img/expansions/gargan-mirklace.jpg';
-import rylanOlivenImg from '../assets/img/expansions/rylan-olliven.jpg';
-import skarnImg from '../assets/img/expansions/skarn.png';
-import ardusIxErebusImg from '../assets/img/expansions/ardus-ix-erebus.png';
-import kyndrithulImg from '../assets/img/expansions/kyndrithul.png';
-import zarihellImg from '../assets/img/expansions/zarihell.png';
+import elizaFarrowImg from '../assets/img/expansions/eliza-farrow.webp';
+import spligImg from '../assets/img/expansions/splig.webp';
+import zacharethImg from '../assets/img/expansions/zachareth.webp';
+import ariadImg from '../assets/img/expansions/ariad.webp';
+import valyndraImg from '../assets/img/expansions/valyndra.webp';
+import queenAriadImg from '../assets/img/expansions/queen-ariad.webp';
+import belthirImg from '../assets/img/expansions/belthir.webp';
+import raythenImg from '../assets/img/expansions/raythen.webp';
+import bolgorethImg from '../assets/img/expansions/bolgoreth.webp';
+import tristayneOlivenImg from '../assets/img/expansions/tristayne-olliven.webp';
+import serenaImg from '../assets/img/expansions/serena.webp';
+import verminousImg from '../assets/img/expansions/verminous.webp';
+import garganMirklaceImg from '../assets/img/expansions/gargan-mirklace.webp';
+import rylanOlivenImg from '../assets/img/expansions/rylan-olliven.webp';
+import skarnImg from '../assets/img/expansions/skarn.webp';
+import ardusIxErebusImg from '../assets/img/expansions/ardus-ix-erebus.webp';
+import kyndrithulImg from '../assets/img/expansions/kyndrithul.webp';
+import zarihellImg from '../assets/img/expansions/zarihell.webp';
+import chainsThatRust from '../assets/img/expansions/chains-that-rust.webp';
+import lostLegends from '../assets/img/expansions/lost-legends.webp';
 // Hero & Monster Collections
-import oathOfTheOutcastImg from '../assets/img/expansions/oath-of-the-outcast.png';
-import crownOfDestinyImg from '../assets/img/expansions/crown-of-destiny.png';
-import guardiansOfDeephallImg from '../assets/img/expansions/guardians-of-deephall.png';
-import visionsOfDawnImg from '../assets/img/expansions/visions-of-dawn.png';
-import bondsOfTheWildImg from '../assets/img/expansions/bonds-of-the-wild.png';
-import treatyOfChampionsImg from '../assets/img/expansions/treaty-of-champions.png';
-import stewardsOfTheSecretImg from '../assets/img/expansions/stewards-of-the-secret.png';
+import oathOfTheOutcastImg from '../assets/img/expansions/oath-of-the-outcast.webp';
+import crownOfDestinyImg from '../assets/img/expansions/crown-of-destiny.webp';
+import guardiansOfDeephallImg from '../assets/img/expansions/guardians-of-deephall.webp';
+import visionsOfDawnImg from '../assets/img/expansions/visions-of-dawn.webp';
+import bondsOfTheWildImg from '../assets/img/expansions/bonds-of-the-wild.webp';
+import treatyOfChampionsImg from '../assets/img/expansions/treaty-of-champions.webp';
+import stewardsOfTheSecretImg from '../assets/img/expansions/stewards-of-the-secret.webp';
 // Co-Op Expansions
-import forgottenSoulsImg from '../assets/img/expansions/forgotten-souls.png';
-import naturesIreImg from '../assets/img/expansions/natures-ire.png';
-import darkElementsImg from '../assets/img/expansions/dark-elements.png';
+import forgottenSoulsImg from '../assets/img/expansions/forgotten-souls.webp';
+import naturesIreImg from '../assets/img/expansions/natures-ire.webp';
+import darkElementsImg from '../assets/img/expansions/dark-elements.webp';
 
 export type ExpansionCategoryType = 'box' | 'lieutenant' | 'hero-monster' | 'co-op' | 'campaign';
 
@@ -51,22 +51,22 @@ export interface ExpansionInterface {
 
 export const EXPANSIONS_LIST: ExpansionInterface[] = [
     // Box Expansions
-    {id: 'conversion-kit', name: 'Conversion Kit', image: conversionKitImg, category: 'box'},
+    // {id: 'conversion-kit', name: 'Conversion Kit', image: conversionKitImg, category: 'box'},
     {id: 'lair-of-the-wyrm', name: 'Lair of the Wyrm', image: lairOfTheWyrmImg, category: 'box'},
     {id: 'labyrinth-of-ruin', name: 'Labyrinth of Ruin', image: labyrinthOfRuinImg, category: 'box'},
     {id: 'the-trollfens', name: 'The Trollfens', image: theTrollFensImg, category: 'box'},
     {id: 'shadow-of-nerekhall', name: 'Shadow of Nerekhall', image: shadowOfNerekhallImg, category: 'box'},
     {id: 'manor-of-ravens', name: 'Manor of Ravens', image: manorOfRavensImg, category: 'box'},
     {id: 'mists-of-bilehall', name: 'Mists of Bilehall', image: mistsOfBilehallImg, category: 'box'},
-    {id: 'chains-that-rust', name: 'The Chains that Rust', image: null, category: 'box'},
-    {id: 'lost-legends', name: 'Lost Legends', image: null, category: 'box'},
+    {id: 'chains-that-rust', name: 'The Chains that Rust', image: chainsThatRust, category: 'box'},
+    {id: 'lost-legends', name: 'Lost Legends', image: lostLegends, category: 'box'},
     // Lieutenant Packs
     {id: 'eliza-farrow', name: 'Eliza Farrow', image: elizaFarrowImg, category: 'lieutenant'},
     {id: 'splig', name: 'Splig', image: spligImg, category: 'lieutenant'},
     {id: 'zachareth', name: 'Baron Zachareth', image: zacharethImg, category: 'lieutenant'},
     {id: 'ariad', name: 'Ariad', image: ariadImg, category: 'lieutenant'},
     {id: 'merick-farrow', name: 'Merick Farrow', image: null, category: 'lieutenant'},
-    {id: 'alric-farrow', name: 'Sir Alric Farrow', image: alricFarrowImg, category: 'lieutenant'},
+    {id: 'alric-farrow', name: 'Sir Alric Farrow', image: null, category: 'lieutenant'},
     {id: 'valyndra', name: 'Valyndra', image: valyndraImg, category: 'lieutenant'},
     {id: 'queen-ariad', name: 'Queen Ariad', image: queenAriadImg, category: 'lieutenant'},
     {id: 'belthir', name: 'Belthir', image: belthirImg, category: 'lieutenant'},
@@ -100,7 +100,6 @@ export const EXPANSIONS_LIST: ExpansionInterface[] = [
     {id: 'forgotten-souls', name: 'Forgotten Souls', image: forgottenSoulsImg, category: 'co-op'},
     {id: 'natures-ire', name: "Nature's Ire", image: naturesIreImg, category: 'co-op'},
     {id: 'dark-elements', name: 'Dark Elements', image: darkElementsImg, category: 'co-op'},
-    {id: 'road-to-legend', name: 'Road to Legend', image: null, category: 'co-op'},
 ];
 
 export const ALL_EXPANSION_NAMES: string[] = EXPANSIONS_LIST.map((e) => e.name);
